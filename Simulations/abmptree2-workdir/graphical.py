a26529318124403bb01b91a909fea69ba1f2238a
Marcello Marques <prodmarcello@gmail.com>
2017-10-13 15:16:39 -0300

    adjustments

--- Simulations/abmptree2-workdir/graphical.py
+++ Simulations/abmptree2-workdir/graphical.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import math
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 #########################################################
@@ -33,15 +34,15 @@ else:
 
 numNodes = 53
 cont = 0
-node_position_x = [0 for i in range(0, numNodes)]
-node_position_y = [0 for i in range(0, numNodes)]
-node_position_z = [0 for i in range(0, numNodes)]
+npx = [0 for i in range(0, numNodes)]
+npy = [0 for i in range(0, numNodes)]
+npz = [0 for i in range(0, numNodes)]
 
 while cont < numNodes:
-	node_position_x[cont], it = getValueCoord(txt, it)
-	node_position_y[cont], it = getValueCoord(txt, it)
-	node_position_z[cont], it = getValueCoord(txt, it)
-	#print node_position_x[cont], node_position_y[cont], node_position_z[cont]
+	npx[cont], it = getValueCoord(txt, it)
+	npy[cont], it = getValueCoord(txt, it)
+	npz[cont], it = getValueCoord(txt, it)
+	#print npx[cont], npy[cont], npz[cont]
 
 	cont += 1
 #########################################################
@@ -50,24 +51,42 @@ def dist(x1, y1, x2, y2):
     return math.sqrt((x1-x2)**2 + (y1-y2)**2)
 
 def interf(ple, d0, pld0, sigma, pt, sen_tr, d):         #prob 95.44
-    return pt - (pld0 + 10*ple*math.log10(d/d0) - sigma/2) >= sen_tr
+    return pt - (pld0 + 10*ple*math.log10(d/d0) - sigma*2) >= sen_tr
 
 ##########################################################
-fig, axScatter = plt.subplots()
 
-# the scatter plot:
-axScatter.scatter(node_position_x[5:], node_position_y[5:], s=50, c="red", alpha = 0.5, marker='1', label="sensor node")
-axScatter.scatter(node_position_x[1:5], node_position_y[1:5], s=100, c="blue", alpha = 0.5, marker='1', label="sink node")
-axScatter.scatter(node_position_x[0], node_position_y[0], s=150, c="green", alpha = 0.5, marker='1', label="router node")
-axScatter.set_aspect(1)
+#levando em consideracao que vou imprimir em papel preto/branco
+
+
+# conseguir valore uteis em omnetpp.ini
+NUM_OF_CHANNELS = 16
+number_ch = 4
+    #poreqnuanto estou setando os valores
+ple = 2.4 #expoente de perda
+pld0 = 80.48 #perda na distancia de referencia
+d0 = 15 #distancia de referencia
+sigma = 6.62 #desvio padrao em dB a ser aplicado
+pt = 0 #potencia de transmissao
+sen_tr = -94 #sensibilidade do transceptor
+
+##############################
+plt.suptitle("Connections and Interferences in IWSN")
+fl1 = fl2 = 0 #flags de legenda
+for i in range(1, 5):
+	for j in range(5, 53):
+		if interf(ple, d0, pld0, sigma, pt, sen_tr, dist(npx[i], npy[i], npx[j], npy[j])):
+			if  i*12 - 7 <= j <= i*12+5:
+				plt.plot([npx[i], npx[j]], [npy[i], npy[j]], c='#000000', alpha = 0.3, label="connection" if fl1 == 0 else ""); fl1 = 1
+			else:
+				plt.plot([npx[i], npx[j]], [npy[i], npy[j]], 'r--', c="#000000", alpha = 0.1, label="interference" if fl2 == 0 else ""); fl2 = 1
+
+plt.axis([min(npx)-5, max(npx)+5, min(npy)-5, max(npy)+5])
+plt.plot(npx[5:], npy[5:], 'ro', ms='8', c="#000000", alpha = 0.8, marker='1', label="sensor node")
+plt.plot(npx[1:5], npy[1:5], 'ro', ms='12', c="#000000", alpha = 0.8, marker='H', label="router node")
+plt.plot(npx[0], npy[0], 'ro', ms='12', c="#000000", alpha = 0.8, marker='$SN$', label="sink node")
 plt.xlabel("position x axis")
 plt.ylabel("position y axis")
 plt.legend(loc = 3)
-
-for i in range(1, 5):
-    for j in range(5, 53):
-    		if interf(i, j)
-
 
 plt.draw()
 plt.show()
